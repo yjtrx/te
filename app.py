5e8452ddcebc0df68affe3a73c42ae13d096aae9
yjtrx <53395264+yjtrx@users.noreply.github.com>
2021-04-02 10:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,request,jsonify,render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('rfc.pkl','rb'))
+model = pickle.load(open('aqt.pkl','rb'))
 
 @app.route('/')
 def home():
